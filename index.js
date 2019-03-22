0804ebc5c95178acab18a659e63ad4ad5fa299f7
Daniele Lasagno <danielelasagno@gmail.com>
2019-03-22 22:26:40 +0100

    Completed best practices rules section

--- index.js
+++ index.js
@@ -1,25 +1,71 @@
 module.exports = {
-    parserOptions: {
-        ecmaVersion: 2019,
-        sourceType: 'module'
-    },
-    env: {
-        es6: true,
-        node: true,
-        browser: true
-    },
-    extends: 'eslint:recommended',
-    rules: {
-        'no-async-promise-executor': [1],
-        'no-console': 0, // To revise in future,
-        'no-extra-parens': [2, 'all', {
-            nestedBinaryExpressions: false,
-            ignoreJSX: 'all',
-            enforceForArrowConditionals: false
-        }],
-        'no-template-curly-in-string': 2,
-        'require-atomic-updates': 2,
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module'
+  },
+  env: {
+    es6: true,
+    node: true,
+    browser: true
+  },
+  extends: 'eslint:recommended',
+  rules: {
+    // Possible errors
+    'no-async-promise-executor': ['warn'],
+    'no-console': 'off', // To revise in future,
+    'no-extra-parens': ['error', 'all', {
+        nestedBinaryExpressions: false,
+        ignoreJSX: 'all',
+        enforceForArrowConditionals: false
+    }],
+    'no-template-curly-in-string': 'error',
+    'require-atomic-updates': 'error',
 
-        'accessor-pairs': 1
-    }
+    // Best practices
+    'accessor-pairs': 'warn',
+    'array-callback-return': 'error',
+    'block-scoped-var': 'error',
+    'class-method-use-this': 'warn',
+    'consistent-return': 'error',
+    'curly': ['error', 'multi', 'consistent'],
+    'dot-location': ['error', 'property'],
+    'dot-notation': 'warn',
+    'eqeqeq': ['error', 'smart'],
+    'no-alert': 'warn',
+    'no-caller': 'warn',
+    'no-case-declarations': 'error',
+    'no-div-regex': 'error',
+    'no-else-return': 'error',
+    'no-empty-function': 'error',
+    'no-eq-null': 'error',
+    'no-eval': 'error',
+    'no-extend-native': 'warn',
+    'no-extra-bind': 'warn',
+    'no-extra-label': 'error',
+    'no-floating-decimal': 'error',
+    'no-implicit-coercion': 'error',
+    'no-implied-eval': 'error',
+    'no-magic-numbers': ['error', { ignore: [-1, 0, 1, 2] }],
+    'no-multi-spaces': ['error', { ignoreEOLComments: true }],
+    'no-multi-str': 'error',
+    'no-octal-escape': 'error',
+    'no-redeclare': ['error', { builtinGlobals: true }],
+    'no-return-assign': 'error',
+    'no-return-await': 'warn',
+    'no-script-url': 'error',
+    'no-self-assign': 'warn',
+    'no-self-compare': 'warn',
+    'no-sequences': 'error',
+    'no-throw-literal': 'error',
+    'no-unmodified-loop-condition': 'warn',
+    'no-unused-labels': 'warn',
+    'no-useless-call': 'warn',
+    'no-useless-catch': 'warn',
+    'no-useless-concat': 'error',
+    'no-useless-return': 'error',
+    'no-void': 'error',
+    'no-with': 'error',
+    'yoda': ['error', 'never', { onlyEquality: true }]
+    
+  }
 }
