5e5c6197267b6f15d36de2903a1cbf3124b756eb
Daniele Lasagno <danielelasagno@gmail.com>
2019-03-23 14:03:32 +0100

    Completed es6 section

--- index.js
+++ index.js
@@ -119,6 +119,28 @@ module.exports = {
     'space-in-parens': 'error',
     'spaced-comment': ['error', 'always'],
     'switch-colon-spacing': 'error',
-    'template-tag-spacing': 'error'
+    'template-tag-spacing': 'error',
+
+    // ECMAScript 6
+    'body-style': 'error',
+    'arrow-parens': ['error', 'as-needed'],
+    'arrow-spacing': 'error',
+    'generator-star-spacing': 'error',
+    'no-confusing-arrow': ['error', { allowParens: true }],
+    'no-duplicate-imports': 'warn',
+    'no-useless-computed-key': 'warn',
+    'no-useless-constructor': 'warn',
+    'no-useless-rename': 'warn',
+    'no-var': 'warn',
+    'object-shortand': 'error',
+    'prefer-arrow-callback': 'error',
+    'prefer-const': 'warn',
+    'prefer-destructuring': ['error', { array: false, object: true }],
+    'prefer-rest-params': 'warn',
+    'prefer-spread': 'warn',
+    'prefer-template': 'error',
+    'rest-spread-spacing': 'error',
+    'symbol-description': 'warn',
+    'yield-star-spacing': 'error'
   }
 }
